4597022c1ddef7dc2b6d30c96f6438fa8f415d98
sandmanx24 <dmarrerox24@gmail.com>
2015-04-22 12:27:56 -0400

    Sample edits for Main class

--- ClassFinal/src/application/Main.java
+++ ClassFinal/src/application/Main.java
@@ -3,17 +3,33 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			// use grid pane
+			GridPane root = new GridPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			
+			// place simple label for testing
+			root.setLayoutX(30);
+			root.setLayoutY(30);
+			root.setHgap(20);
+			root.setVgap(20);
+			Label lblPetName = new Label("Pet Name:");
+			root.add(lblPetName, 0, 0);
+			TextField txtPetName = new TextField();
+			root.add(txtPetName, 1, 0);
+			
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
